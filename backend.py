a69340a62d18b3530674680c0eceb027dd201610
Andressa-Melo <andressamelobw6@gmail.com>
2024-10-31 21:34:35 -0300

    Adicionei o primeiro html dentro de uma função no flask, obs: ainda estamos com link local

--- backend.py
+++ backend.py
@@ -1,23 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template  #com jsonify dá para converter valores em javascript para uma String JSON
 
 app=Flask(__name__,template_folder='template')
-# route -> hashtagtreinamentos.com/
-# função -> o que você quer exibir naquela página
-# template
 
-@app.route("/")
+@app.route("/")  #primeira pagina (homepage)
 def homepage():
     return "hahaaaaaaaaaaaaaaaaaaaaaauuuuu"
 
 
 @app.route("/nivel1")
 def primeironivel():
-    return render_template("network.html")
-
-
+    return render_template("network.html") # render_template busca a pasta template onde deve estar os html
 
 # colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
-
-    # servidor do heroku
